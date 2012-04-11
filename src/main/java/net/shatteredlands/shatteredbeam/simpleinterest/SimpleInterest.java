c787426f8912dcba00f6203fefff2968f451e381
shatteredbeam <shatteredbeam@gmail.com>
2012-04-11 16:42:02 -0700

    Add beginnings of Command processor

--- src/main/java/net/shatteredlands/shatteredbeam/simpleinterest/SimpleInterest.java
+++ src/main/java/net/shatteredlands/shatteredbeam/simpleinterest/SimpleInterest.java
@@ -2,17 +2,26 @@ package net.shatteredlands.shatteredbeam.simpleinterest;
 
 
 import java.util.logging.Logger;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import net.milkbowl.vault.economy.Economy;
-import net.milkbowl.vault.permission.Permission;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
 
 public class SimpleInterest extends JavaPlugin implements Listener {
 	private static final Logger log = Logger.getLogger("Minecraft");
 	public static Permission perms = null;
 	public static Economy econ = null;
+	
+	//Command Handling
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+	public void onCommandPreprocess(PlayerCommandPreprocessEvent event) {
+		if(event.getMessage().toLowerCase().startsWith("/si")) return;
+		}
 	
 	@Override
 	public void onEnable() {
@@ -56,16 +65,13 @@ public class SimpleInterest extends JavaPlugin implements Listener {
 	 
 	 private void processInterest() {
 		 double bal, gained;
-		
 		 
 		 for(Player player : getServer().getOnlinePlayers()) {			 
 			 	bal = econ.getBalance(player.getPlayerListName());
-			 	gained = bal * Config.interest;
+			 	gained = Math.floor((bal * Config.interest));
+			 	player.sendMessage("* gained " + gained + " " + econ.currencyNamePlural() + " in Interest.");
 			 	econ.depositPlayer(player.getPlayerListName(), gained);
 			 	log.info("Player " + player.getPlayerListName() + " gained " + gained + " " + econ.currencyNamePlural() + " in Interest.");
-			}
-	
-		
-	 }
-
+		}
+	}
 }
